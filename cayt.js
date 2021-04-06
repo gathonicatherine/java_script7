9e07d977baffa12ca2f299d86e149afbb63aebb4
gathonicatherine <catherrinewanjiku@gmail.com>
2021-04-06 09:08:27 +0300

    java_scrip7

---
+++ cayt.js
@@ -0,0 +1,37 @@
+class KioskCalc{
+ constructors(fruits,quality){ 
+    this.fruits = "Mango";
+    this.price= 15;
+    this.quality = 2;
+
+    this.fruits = "Orange";
+    this.price = 30;
+    this.quality = 1;
+
+    this.fruits = "Avocado";
+    this.price= 40;
+    this.quality = 2;
+
+
+ }
+ getTotalCost(){
+     return "${this.quality}${this.fruits}for KES ${this.price*this.quality}";
+ }
+ getTotalCost(){
+     return "${this.quality}${this,fruits}foe KES ${this.price1*this.quality1}";
+ }
+ getTotalCost(){
+    return "${this.quality}${this,fruits}foe KES ${this.price2*this.quality2}";
+ }
+}
+var fruit = new fruits("Mango,2");
+console.log(fruits.getTotalCost());
+
+var fruit = new fruits("Orange",1);
+console.log(friuts1.getTotalCost1());
+
+var fruit = new fruits("Avocado", 2);
+console.log(fruits2.getTotalCost2());
+
+
+
